470b853379c12a1df6dfe721244fe15f4c69121d
codepraycode <preciousolusola16@gmail.com>
2023-10-06 05:51:54 +0100

    Updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
                         <li>
                            <Icon
                             name='github'
+                            link='https://github.com/orunto/iostream'
                            />
                         </li>
                     </ul>
@@ -49,7 +50,43 @@ export default function Home() {
 
 
 
-                <section className='d-flex col row-md justify-evenly align-center my-10'>
+                <section className='my-10'>
+
+                    <h2 className='text-center text-lg2'>Information is a tributary to control</h2>
+                    <p className='text-center text-lg5 grey mt-2'>Stay ahead of the curve with our real-time alerting feature.<br/>Explore how our IoT monitoring system keeps you informed and in control.</p>
+
+
+                    <div className='features'>
+                        <article>
+                            <h3 className='text-lg5'>Real-time Data Streams</h3>
+                            <p className='text-lg6 mt-1'>Continuous data collection and streaming of information from IoT devices and sensors, Provides a constant flow of data, allowing for immediate insights and decision-making</p>
+                        </article>
+
+
+                        <article>
+                            <h3 className='text-lg5'>Alerts and Notifications</h3>
+                            <p className='text-lg6 mt-1'>Instant notification system that triggers alerts when predefined conditions or thresholds are met. Enables timely responses to critical events or anomalies, helping maintain control over operations.</p>
+                        </article>
+
+
+                        <article>
+                            <h3 className='text-lg5'>Centralized Data Management</h3>
+                            <p className='text-lg6 mt-1'>Aggregates and stores data from various IoT sources in a centralized platform. Facilitates easy access to historical data for analysis and decision support, contributing to better control.</p>
+                        </article>
+
+
+                        <article>
+                            <h3 className='text-lg5'>Remote Monitoring and Control</h3>
+                            <p className='text-lg6 mt-1'>Enables remote access and control of IoT devices and systems. Empowers users to make real-time adjustments, improving control over processes or assets regardless of location.</p>
+                        </article>
+
+                        
+                    </div>
+
+                </section>
+
+
+                <section className='d-flex col row-md justify-evenly align-center mt-10'>
                     
                     <h2 className='text-lg3'>Built with the latest cloud storage technology</h2>
 
