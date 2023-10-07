614728877aef15108c27b2b8aabadeb8c7199aeb
codepraycode <preciousolusola16@gmail.com>
2023-10-07 15:50:42 +0100

    Updated log time verbose

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,13 +1,12 @@
 // What is the distance from 1 January 2016
 
-import { formatDistance } from "date-fns";
+import { formatDistanceToNowStrict } from "date-fns";
 
 // to 1 January 2015, with a suffix?
 
 export function verboseTime(date:Date) {
 
-    const result = formatDistance(date, new Date(), {
-      includeSeconds: true,
+    const result = formatDistanceToNowStrict(date, {
       addSuffix: true
     });
 
